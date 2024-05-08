f597a0f4a40e26255c94868b86f185ef5787a183
starwolfaw <evdryakin@gmail.com>
2024-05-08 13:04:49 +0300

    8

--- 13/main.py
+++ 13/main.py
@@ -1,10 +1,13 @@
-import nltk
-from nltk.tokenize import word_tokenize
+##import nltk
+##from nltk.tokenize import word_tokenize
 import re
+from collections import Counter
 file = open('txtw', 'r').read()
-nltk.download('punkt')
+##nltk.download('punkt')
 print(file.split())
 token = re.findall("[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+", file)
 print(token)
-nldo = word_tokenize(file)
-print(nldo)
+print(len(token))
+print(Counter(token))
+##nldo = word_tokenize(file)
+##print(nldo)
