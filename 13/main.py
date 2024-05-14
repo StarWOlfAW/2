b14844d497e3442633989a3fe841351ebc76fbb9
StarWolfAW <evdryakin@gmail.com>
2024-05-14 20:16:15 +0300

    9

--- 13/main.py
+++ 13/main.py
@@ -1,13 +1,41 @@
-##import nltk
-##from nltk.tokenize import word_tokenize
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 import re
 from collections import Counter
-file = open('txtw', 'r').read()
-##nltk.download('punkt')
-print(file.split())
-token = re.findall("[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+", file)
+from nltk.stem import WordNetLemmatizer
+from nltk.probability import FreqDist
+import matplotlib.pyplot
+import spacy
+
+
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+file = open('txtw.txt', 'r').read()
+opt = re.sub(r'[^\w\s]','', file)
+nlp = spacy.load('en_core_web_sm')
+nltk.download('wordnet')
+print(opt.split())
+tokend = re.findall("[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+", opt)
+token = word_tokenize(opt)
+wistop = [word for word in token if not word in stopwords.words('english')]
+stemmed_words = [stemmer.stem(word) for word in token]
+lemmatized_words = [lemmatizer.lemmatize(word) for word in token]
 print(token)
 print(len(token))
 print(Counter(token))
-##nldo = word_tokenize(file)
-##print(nldo)
+fdist = FreqDist(token)
+fdist.most_common(5)
+certain = FreqDist(wistop)
+certain.plot(30,cumulative=False)
+doc = nlp(opt)
+for tokens in doc:
+    print(tokens.text, tokens.lemma_)
+
+
+nldo = word_tokenize(opt)
+print(nldo)
+print(wistop)
+print(stemmed_words)
+print(lemmatized_words)
